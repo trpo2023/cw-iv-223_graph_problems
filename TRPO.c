2b0eedea5d15103cd3be41a8de037eb65a93d6ef
Timofey Kondratiev <kotim4444@gmail.com>
2023-05-17 17:59:33 +0700

    Create Longest Path Programm

--- TRPO.c
+++ TRPO.c
@@ -1,103 +1,43 @@
+#include <limits.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define V 10
-#define INT_MAX 1000000000
-
-void LongestPath(int** g, int src, int* D, int* pos, int* prev);
-
-void print(int** g, int* D, int src, int* prev, int dst, int* pos)
-{
-    LongestPath(g, src, D, pos, prev);
-    printf("Расстояние от вершины %d до всех остальных\n", src + 1);
-    for (int i = 0; i < V; ++i) {
-        printf("%d - %d : %d\n", src + 1, i + 1, D[i]);
-    }
-    /*for (int i = 0, z = 1; i < V; ++i) {
-        printf("%d : %d\n", z++, prev[i] + 1);
-    }*/
-    printf("\n");
-    int i = dst, count = 0;
-    while (prev[i] != prev[src]) {
-        i = prev[i];
-        count++;
-    }
-    i = dst;
-    int j = 1, path[count];
-    while (prev[i] != prev[src]) {
-        path[count - j++] = i + 1;
-        i = prev[i];
-    }
-    printf("Длиннейший путь из вершины %d в вершину %d\n%d ", src + 1, dst + 1, src + 1);
-    for (int i = 0; i < count; ++i) {
-        printf("%d ", path[i]);
-    }
-    printf("\n");
+#define V 9
+int minDistance(int dist[], bool sptSet[]) {
+   int min = INT_MAX, min_index;
+   for (int v = 0; v < V; v++)
+   if (sptSet[v] == false && dist[v] <= min)
+      min = dist[v], min_index = v;
+   return min_index;
 }
-
-int PriorityQueueExtractMax(int* D, int* pos)
-{
-    int max = INT_MAX, max_index;
-    for (int i = 0; i < V; ++i) {
-        if (D[i] > max) {
-            max = D[i];
-            max_index = i;
-        }
-    }
-    return max_index;
+int printSolution(int dist[], int n) {
+   printf("Vertex Distance from Source\n");
+   for (int i = 0; i < V; i++)
+      printf("%d \t %d\n", i, dist[i]);
 }
-
-void LongestPath(int** g, int src, int* D, int* pos, int* prev)
-{
-    for (int i = 0; i < V; ++i) {
-        D[i] = INT_MAX;
-        pos[i] = -1;
-        prev[i] = -1;
-    }
-    D[src] = 0;
-    for (int i = 0; i < V - 1; ++i) {
-        int u = PriorityQueueExtractMax(D, pos);
-        pos[u] = 1;
-        for (int v = 0; v < V; ++v) {
-            if (D[u] + g[u][v] < D[v]) {
-                D[v] = D[u] + g[u][v];
-                prev[v] = u;
-            }
-        }
-    }
+void dijkstra(int graph[V][V], int src) {
+   int dist[V];
+   bool sptSet[V];
+   for (int i = 0; i < V; i++)
+      dist[i] = INT_MAX, sptSet[i] = false;
+      dist[src] = 0;
+   for (int count = 0; count < V - 1; count++) {
+      int u = minDistance(dist, sptSet);
+      sptSet[u] = true;
+      for (int v = 0; v < V; v++)
+         if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]) dist[v] = dist[u] + graph[u][v];
+   }
+   printSolution(dist, V);
 }
-
-int main()
-{
-    srand(time(NULL));
-    int** g = malloc(sizeof(int*) * V);
-    for (int i = 0; i < V; ++i) {
-        g[i] = malloc(sizeof(int) * V);
-    }
-    for (int i = 0; i < V; ++i) {
-        for (int j = 0; j < V; ++j) {
-            g[i][j] = INT_MAX;
-        }
-    }
-    for (int i = 0; i < V - 1; ++i) {
-        for (int j = 0; j < V; ++j) {
-            if (i != j) {
-                g[i][j] = 1 + rand() % 100;
-            }
-        }
-    }
-    int* D = malloc(sizeof(int) * V);
-    int* pos = malloc(sizeof(int) * V);
-    int* prev = malloc(sizeof(int) * V);
-    int src = 0, dst = 9;   // src - от какой вершины смотрим
-    print(g, D, src, prev, dst, pos); // dst - вершина у которой смотрим путь
-    free(D);
-    free(pos);
-    free(prev);
-    for (int i = 0; i < V; ++i) {
-        free(g[i]);
-    }
-    free(g);
-    return 0;
+int main() {
+   int graph[V][V] = { { 0, 6, 0, 0, 0, 0, 0, 8, 0 },
+      { 6, 0, 8, 0, 0, 0, 0, 13, 0 },
+      { 0, 8, 0, 7, 0, 6, 0, 0, 2 },
+      { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+      { 0, 0, 6, 14, 10, 0, 2, 0, 0 },
+      { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+      { 8, 13, 0, 0, 0, 0, 1, 0, 7 },
+      { 0, 0, 2, 0, 0, 0, 6, 7, 0 }
+   };
+   dijkstra(graph, 0);
+   return 0;
 }
