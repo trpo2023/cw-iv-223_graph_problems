e560239a7cfea0d67c6dae10fb84e67996d2cce1
Kirill Shitov <Kirill9900@Shitov.Kirill.04@mail.ru>
2023-05-20 01:28:59 +0700

    editing input validation

---
+++ src/graph.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdbool.h>
+
+void bfs(int** g, int v, bool* pos, int* prev, int V);
+void dfs(int** g, bool* pos, int start, int* prev, int V);
+void Number_of_paths(int** g, int src, int dst, bool* pos, int V, int* count);
+int PriorityQueueExtractMin(int* D, bool* pos, int V);
+void ShortestPathDijkstra(
+        int** g, int src, int* D, bool* pos, int* prev, int V);
+void print(int** g, int* D, int src, int* prev, int dst, bool* pos, int V);
+
